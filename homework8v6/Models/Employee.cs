12ae34ced96d493d1e2450aa87b0f54707ca1866
Ilya Roshupkin <roshupkin.ilya2012@yandex.ru>
2020-08-22 10:27:47 +0300

    homework8

---
+++ homework8v6/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace homework8v6.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string FullName { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Department { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Position { get; set; }
+    }
+}
